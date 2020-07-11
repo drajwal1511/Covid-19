4eb7005b858034f249874657cf349152199d8783
Dikshant Rajwal <drajwal1511@gmail.com>
2020-07-11 22:10:14 +0530

    added summary to world

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const Router = express.Router();
 var http = require("https");
-
 Router.get("/",function(req,res){
     res.render("landingpage");
 })
@@ -27,7 +26,26 @@ Router.get("/covid",function(req,res){
             pbody=JSON.parse(body);
             if(pbody[0]){
                 var imgsrc = "https://images.vexels.com/media/users/3/140409/isolated/preview/bb6e7b0f2c4e29df1f8e628d41f47eb5-globe-round-icon-1-by-vexels.png";
-                res.render("home",{img:imgsrc,body:JSON.parse(body)});
+                var ops = {
+                    "method": "GET",
+                    "hostname": "api.covid19api.com",
+                    "port": null,
+                    "path": "/summary",
+                    "headers": {
+                    }
+                };    
+                var sreq = http.request(ops, function (sres) {
+                    var schunks = [];    
+                    sres.on("data", function (schunk) {
+                        schunks.push(schunk);
+                    });    
+                    sres.on("end", function () {
+                        var sbody = Buffer.concat(schunks);
+                        psbody=JSON.parse(sbody);
+                        res.render("home",{img:imgsrc,body:JSON.parse(body),summary:psbody});
+                    });
+                });    
+                sreq.end();
             }
             else{
                 res.send("Not Found");
